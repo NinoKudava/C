e1d130dacd0b37384448c817f1ad6652339c6f11
NinoKudava <nk1725@nyu.edu>
2019-01-29 22:42:00 -0500

    Create test9.c
    //C program that generates 50 random numbers between -0.5 and 0.5 and writes them in a file rand.dat.  The first line of ran.dat contains the number of data and the next 50 lines contains the 50 random numbers.

---
+++ test9.c
@@ -0,0 +1,38 @@
+//C program that generates 50 random numbers between -0.5 and 0.5 and writes them in a file rand.dat.
+//The first line of ran.dat contains the number of data and the next 50 lines contains the 50 random numbers.
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#define N 50
+
+int main(){
+    int i;
+    char str;
+    FILE * fptr;
+    fptr = fopen("rand.dat","w");
+    if (fptr == NULL){
+        printf("Error in creating output.dat\n");
+        return 0;
+    }
+
+    srand(time(NULL));
+    fprintf(fptr, "%d\n", N);
+    for(i=1; i<=N; i++){
+        fprintf(fptr, "%0.4lf\n", (rand() % 2001 - 1000) / 2.e3);
+
+    }
+
+    fclose(fptr);
+    fptr = fopen("rand.dat", "r");
+    str = fgetc(fptr);
+
+    while (str != EOF){
+        printf("%c", str);
+        str = fgetc(fptr);
+
+    }
+
+    fclose(fptr);
+    return 0;
+}
